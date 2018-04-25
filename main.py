265143b5e29535b4e500c7e7a5935e8d40b8ee5d
mgalvc <matheusgcorreia@gmail.com>
2018-04-24 22:32:20 -0300

    change M to A

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import LU
 import numpy as np
 
-M = [
+A = [
     [10,2,1,7],
     [1,5,1,-8],
     [2,3,10,6]
 ]
 
-LU.solve(np.array(M, dtype=np.float64))
+LU.solve(np.array(A, dtype=np.float64))
